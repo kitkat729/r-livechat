f8493f15fbf9b4dafb1073a4aaf09d6c02b367f5
Hoi Yuen <hoikityuen@gmail.com>
2018-04-17 21:32:51 -0400

    integrate-redux simplified bootstrap file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import './index.css'
+import Root from './containers/Root'
+import registerServiceWorker from './registerServiceWorker'
 
-var win1 = {
-  sender: {
-    id: '123',
-    name: 'Laura'
-  },
-  recipient: {
-    id: '456',
-    name: 'Rob'
-  }
-}
 
-var win2 = {
-   sender: {
-      id: '456',
-      name: 'Rob'    
-  },
-  recipient: {
-      id: '123',
-      name: 'Laura'    
-  } 
-}
-
-ReactDOM.render(
-    <div className="flex-container chat-app">
-      <App session={win1} />
-      <App session={win2} />
-    </div>, document.getElementById('root'));
+render(
+  <Root />,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
