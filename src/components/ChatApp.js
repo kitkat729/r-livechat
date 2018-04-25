e543c974c10b0890fa11088febd088a95fb8993f
Hoi Yuen <hoikityuen@gmail.com>
2018-04-25 03:52:28 -0400

    add-sass-support copied css style to its new scss file unmodified. Updated stylesheet reference

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-import '../App.css'
+import '../app.scss'
 import ChatPane from '../containers/ChatPane'
 
 const ChatApp = ({chatSession}) => {
